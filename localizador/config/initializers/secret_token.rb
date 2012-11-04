36527a5198b8f66ba8e2de7449fda15fbafe0748
Fernando Vitale <fpvvitale@gmail.com>
2012-11-04 08:19:11 -0300

    removed test files

---
+++ localizador/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Localizador::Application.config.secret_token = 'dcae48d100664e0f142ed6d9f6af8a281eb88bb8857ec87ee3c936f2369898fb1d80d6726d68d8487f5a8150b146360a798fa15b3cc7825c67312a2090e64233'
